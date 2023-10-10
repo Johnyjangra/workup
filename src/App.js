5b1c8a0e6337ae56efd16acefeea11181c3b9c9e
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-10 10:06:45 +0530

    add routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Feature from "./components/home/Feature";
 // import Hero from "./components/home/Hero";
 import OurMisson from "./components/home/OurMisson";
 import SoluctionSucces from "./components/home/SoluctionSucces";
+import Homepage from "./views/Homepage";
+import ChatRoom from "./views/ChatRoom";
+import Founder from "./views/Founder";
+import Brand from "./views/Brand";
 
 function App() {
   return (
     <>
       {/* <Hero /> */}
-      <Feature/>
+      {/* <Feature/>
       <OurMisson />
-      <SoluctionSucces/>
+      <SoluctionSucces /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/chatroom" element={<ChatRoom />} />
+          <Route path="/founder" element={<Founder />} />
+          <Route path="/brand" element={<Brand />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
